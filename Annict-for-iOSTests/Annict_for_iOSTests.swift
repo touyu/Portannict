ef2575745ec920daa5a94c9af0c6d45fc31cab79
touyu <akkey0222@gmail.com>
2017-02-10 13:04:49 +0900

    Initial Commit

---
+++ Annict-for-iOSTests/Annict_for_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Annict_for_iOSTests.swift
+//  Annict-for-iOSTests
+//
+//  Created by Yuto Akiba on 2017/02/10.
+//  Copyright © 2017年 Yuto Akiba. All rights reserved.
+//
+
+import XCTest
+@testable import Annict_for_iOS
+
+class Annict_for_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
