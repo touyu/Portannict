d62703636d091619b82cdfefee6611803e57ceb5
touyu <akkey0222@gmail.com>
2017-02-12 00:29:08 +0900

    [fix] AnnictをAnnictAPIに変更

---
+++ Annict-for-iOS/AnnictAPI.swift
@@ -0,0 +1,117 @@
+//
+//  AnnictAPI.swift
+//  Annict-for-iOS
+//
+//  Created by Yuto Akiba on 2017/02/10.
+//  Copyright © 2017年 Yuto Akiba. All rights reserved.
+//
+
+import APIKit
+import Himotoki
+
+
+// MARK: - AnnictAPI
+
+final class AnnictAPI: Session {
+
+    struct OauthToken: AnnictAPIRequest {
+        var code: String
+        
+        typealias Response = AnnictOauthTokenResponse
+        
+        var method: HTTPMethod {
+            return .post
+        }
+        
+        var path: String {
+            return "/oauth/token"
+        }
+        
+        var parameters: Any? {
+            return [
+                "client_id": AnnictConsts.clientID,
+                "client_secret": AnnictConsts.clientSecret,
+                "grant_type": "authorization_code",
+                "redirect_uri": AnnictConsts.redirectURI,
+                "code": code
+            ]
+        }
+    }
+    
+    class GetWorks: AnnictAPIRequest {
+        enum Sort: String {
+            case id = "sort_id"
+            case season = "sort_season"
+            case watchersCount = "sort_watchers_count"
+        }
+        
+        enum Order: String {
+            case ascending = "asc"
+            case descending = "desc"
+        }
+        
+        enum FilterStatus: String {
+            case wannaWatch = "wanna_watch"
+            case watching = "watching"
+            case watched = "watched"
+            case onHold = "on_hold"
+            case stopWatching = "stop_watching"
+        }
+        
+        enum Season: String {
+            case spring = "spring"
+            case summer = "summer"
+            case autumn = "autumn"
+            case winter = "winter"
+        }
+        
+        var page: Int
+        var sort: Sort
+        var order: Order
+        
+        var filterStatus: FilterStatus?
+        var filterSeason: (year: Int, season: Season?)?
+        
+        init(filterStatus: FilterStatus? = nil,
+             filterSeason: (year: Int, season: Season?)? = nil) {
+            self.page = 1
+            self.sort = .season
+            self.order = .descending
+            self.filterStatus = filterStatus
+            self.filterSeason = filterSeason
+        }
+        
+        typealias Response = AnnictWorksResponse
+        
+        var method: HTTPMethod {
+            return .get
+        }
+        
+        var path: String {
+            return "/v1/me/works"
+        }
+        
+        var parameters: Any? {
+            var params:[String: Any] = [
+                "access_token": AnnictConsts.accessToken,
+                "page": page,
+                "per_page": 30,
+                sort.rawValue: order.rawValue,
+                ]
+            if let filterStatus = filterStatus {
+                params["filter_status"] = filterStatus.rawValue
+            }
+            if let filterSeason = filterSeason {
+                let season = filterSeason.season?.rawValue ?? "all"
+                params["filter_season"] = "\(filterSeason.year)-\(season)"
+            }
+            return params
+        }
+    }
+    
+    final class GetMeWorks: GetWorks {
+        override var path: String {
+            return "/v1/me/works"
+        }
+    }
+}
