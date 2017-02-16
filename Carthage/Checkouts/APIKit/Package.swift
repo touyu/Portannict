1cf8567f1f3a8cc3a5955075015ef772e06e0071
touyu <akkey0222@gmail.com>
2017-02-16 23:47:29 +0900

    [git] .gitignore is now working

--- Carthage/Checkouts/APIKit/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "APIKit",
-    dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
-    ],
-    exclude: ["Sources/APIKit/BodyParameters/AbstractInputStream.m"]
-)
