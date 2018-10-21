cf2f4faa3a23aeeadc8db9cbf6ae3769f68c57ef
touyu <dev.akkey0222@gmail.com>
2018-10-21 23:19:39 +0900

    add new files

---
+++ Annict-for-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Annict-for-iOS
+//
+//  Created by Yuto Akiba on 2018/10/21.
+//  Copyright © 2018 Yuto Akiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
