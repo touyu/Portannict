1c3d0c5c09bd71f31126cdd1e86bbbda8e49e875
touyu <dev.akkey0222@gmail.com>
2019-03-18 00:40:54 +0900

    Add SwiftDate

--- Annict-for-iOS/Modules/Home/Views/ActivityRecordTableViewCell.swift
+++ Annict-for-iOS/Modules/Home/Views/ActivityRecordTableViewCell.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import SwiftDate
 
 final class ActivityRecordTableViewCell: UITableViewCell {
 
     @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var messageLabel: UILabel!
     @IBOutlet private weak var workAndEpisodeQuoteView: WorkAndEpisodeQuoteView!
 
@@ -38,6 +41,8 @@ final class ActivityRecordTableViewCell: UITableViewCell {
         let user = activityItem.user.fragments.minimumUser
         avatarImageView.setImage(url: user.avatarUrl)
         nameLabel.text = user.name
+        usernameLabel.text = "@" + user.username
+        timeLabel.text = activityItem.createdAt.toDate()?.toRelative()
         messageLabel.text = activityItem.comment
         workAndEpisodeQuoteView.configure(work: activityItem.work.fragments.minimumWork,
                                           episode: activityItem.episode.fragments.minimumEpisode)
