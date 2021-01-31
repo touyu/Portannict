a850d43d9914d97e5144c42b14778f49cdad1f1e
Yuto Akiba <dev.akkey0222@gmail.com>
2021-01-31 19:52:35 +0900

    Impl

--- Sources/Common/API/APIClient.swift
+++ Sources/Common/API/APIClient.swift
@@ -74,21 +74,3 @@ final class APIClient {
         return apolloClient
     }()
 }
-
-protocol ServiceType {
-    func fetchHomeActivities(first: Int?, after: String?) -> Effect<GetFollowingActivitiesQuery.Data, APIError>
-}
-
-struct Service: ServiceType {
-    func fetchHomeActivities(first: Int? = nil, after: String? = nil) -> Effect<GetFollowingActivitiesQuery.Data, APIError> {
-        let query = GetFollowingActivitiesQuery(first: first, after: after)
-        return APIClient.shared.fetchEffect(query: query)
-    }
-}
-
-struct PreviewService: ServiceType {
-    func fetchHomeActivities(first: Int?, after: String?) -> Effect<GetFollowingActivitiesQuery.Data, APIError> {
-        return .none
-    }
-}
-
