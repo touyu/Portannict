ae02e05240777c88dd466e9b7d6f80a3149f22db
Yuto Akiba <dev.akkey0222@gmail.com>
2021-03-09 00:02:14 +0900

    Fix

--- Sources/Work/WorkEpisodesView.swift
+++ Sources/Work/WorkEpisodesView.swift
@@ -23,7 +23,7 @@ struct WorkEpisodesState: Equatable {
 
 
     var work: WorkFragment
-    var episodes: [EpisodeFragment] = []
+    var episodeCellStates: [WorkEpisodeCellState] = []
     var pageInfo: PageInfoFragment?
     var isEpisodesLoading: Bool = false
     var presentation: Presentation?
@@ -38,72 +38,85 @@ enum WorkEpisodesAction: Equatable {
 
     case setEpisodes(Result<SearchWorkEpisodesQuery.Data.SearchWork.Node.Episode, APIError>)
     case appendEpisodes(Result<SearchWorkEpisodesQuery.Data.SearchWork.Node.Episode, APIError>)
+
+    case episodeCell(index: Int, action: WorkEpisodeCellAction)
 }
 
 struct WorkEpisodesEnvironment {
     var mainQueue: AnySchedulerOf<DispatchQueue>
 }
 
-let workEpisodesReducer = Reducer<WorkEpisodesState, WorkEpisodesAction, WorkEpisodesEnvironment> { state, action, env in
-    struct RequestId: Hashable {}
+let workEpisodesReducer = Reducer<WorkEpisodesState, WorkEpisodesAction, WorkEpisodesEnvironment>.combine(
+    workEpisodeCellReducer.forEach(state: \.episodeCellStates,
+                                   action: /WorkEpisodesAction.episodeCell(index:action:),
+                                   environment: { _ in  WorkEpisodeCellEnvironment() }),
+    Reducer { state, action, env in
+        struct RequestId: Hashable {}
 
-    func fetchEpisodes() -> Effect<SearchWorkEpisodesQuery.Data.SearchWork.Node.Episode, APIError> {
-        return APIClient.shared.fetchEffect(query: SearchWorkEpisodesQuery(workAnnictId: state.work.annictId, first: 5))
-            .compactMap { $0.searchWorks?.nodes?.first??.episodes }
-            .eraseToEffect()
-    }
+        func fetchEpisodes() -> Effect<SearchWorkEpisodesQuery.Data.SearchWork.Node.Episode, APIError> {
+            return APIClient.shared.fetchEffect(query: SearchWorkEpisodesQuery(workAnnictId: state.work.annictId, first: 5))
+                .compactMap { $0.searchWorks?.nodes?.first??.episodes }
+                .eraseToEffect()
+        }
 
-    func fetchMoreEpisodes() -> Effect<SearchWorkEpisodesQuery.Data.SearchWork.Node.Episode, APIError> {
-        guard let pageInfo = state.pageInfo else { return .none }
-        guard pageInfo.hasNextPage else { return .none }
-        return APIClient.shared.fetchEffect(query: SearchWorkEpisodesQuery(workAnnictId: state.work.annictId,
-                                                                           first: 30,
-                                                                           after: pageInfo.endCursor))
-            .compactMap { $0.searchWorks?.nodes?.first??.episodes }
-            .eraseToEffect()
-    }
+        func fetchMoreEpisodes() -> Effect<SearchWorkEpisodesQuery.Data.SearchWork.Node.Episode, APIError> {
+            guard let pageInfo = state.pageInfo else { return .none }
+            guard pageInfo.hasNextPage else { return .none }
+            return APIClient.shared.fetchEffect(query: SearchWorkEpisodesQuery(workAnnictId: state.work.annictId,
+                                                                               first: 30,
+                                                                               after: pageInfo.endCursor))
+                .compactMap { $0.searchWorks?.nodes?.first??.episodes }
+                .eraseToEffect()
+        }
 
-    switch action {
-    case .fetch:
-        let loading = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(true))
-        let fetchStream = fetchEpisodes()
-            .receive(on: env.mainQueue)
-            .catchToEffect()
-            .map(WorkEpisodesAction.setEpisodes)
-            .cancellable(id: RequestId())
-        let finished = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(false))
-        return Effect.concatenate(loading, fetchStream, finished)
-    case .fetchMore:
-        let loading = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(true))
-        let fetchMoreStream = fetchMoreEpisodes()
-            .receive(on: env.mainQueue)
-            .catchToEffect()
-            .map(WorkEpisodesAction.appendEpisodes)
-        let finished = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(false))
-        return Effect.concatenate(loading, fetchMoreStream, finished)
-    case .setEpisodes(.success(let episodes)):
-        state.episodes = episodes.edges?.compactMap { $0?.node?.fragments.episodeFragment } ?? []
-        state.pageInfo = episodes.pageInfo.fragments.pageInfoFragment
-        return .none
-    case .episodeCellTapped(let index):
-        state.presentation = .episode(state.episodes[index])
-        return .none
-    case .episodeDismissed:
-        state.presentation = nil
-        return .none
-    case .setIsEpisodesLoading(let isLoading):
-        state.isEpisodesLoading = isLoading
-        return .none
-    case .setEpisodes(.failure(let error)):
-        return .none
-    case .appendEpisodes(.success(let episodes)):
-        state.episodes += episodes.edges?.compactMap { $0?.node?.fragments.episodeFragment } ?? []
-        state.pageInfo = episodes.pageInfo.fragments.pageInfoFragment
-        return .none
-    case .appendEpisodes(.failure(let error)):
-        return .none
+        switch action {
+        case .fetch:
+            let loading = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(true))
+            let fetchStream = fetchEpisodes()
+                .receive(on: env.mainQueue)
+                .catchToEffect()
+                .map(WorkEpisodesAction.setEpisodes)
+                .cancellable(id: RequestId())
+            let finished = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(false))
+            return Effect.concatenate(loading, fetchStream, finished)
+        case .fetchMore:
+            let loading = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(true))
+            let fetchMoreStream = fetchMoreEpisodes()
+                .receive(on: env.mainQueue)
+                .catchToEffect()
+                .map(WorkEpisodesAction.appendEpisodes)
+            let finished = Effect<WorkEpisodesAction, Never>(value: .setIsEpisodesLoading(false))
+            return Effect.concatenate(loading, fetchMoreStream, finished)
+        case .episodeCellTapped(let index):
+            //        state.presentation = .episode(state.episodes[index])
+            return .none
+        case .episodeDismissed:
+            state.presentation = nil
+            return .none
+        case .setIsEpisodesLoading(let isLoading):
+            state.isEpisodesLoading = isLoading
+            return .none
+        case .setEpisodes(.success(let episodes)):
+            state.episodeCellStates = episodes.edges?
+                .compactMap { $0?.node?.fragments.episodeFragment }
+                .map { WorkEpisodeCellState(episode: $0) } ?? []
+            state.pageInfo = episodes.pageInfo.fragments.pageInfoFragment
+            return .none
+        case .setEpisodes(.failure(let error)):
+            return .none
+        case .appendEpisodes(.success(let episodes)):
+            state.episodeCellStates += episodes.edges?
+                .compactMap { $0?.node?.fragments.episodeFragment }
+                .map { WorkEpisodeCellState(episode: $0) } ?? []
+            state.pageInfo = episodes.pageInfo.fragments.pageInfoFragment
+            return .none
+        case .appendEpisodes(.failure(let error)):
+            return .none
+        case .episodeCell:
+            return .none
+        }
     }
-}
+)
 
 struct WorkEpisodesView: View {
     let store: Store<WorkEpisodesState, WorkEpisodesAction>
@@ -114,18 +127,10 @@ struct WorkEpisodesView: View {
                 Text("Episodes \(viewStore.work.episodesCount)")
                     .font(.title2)
                     .fontWeight(.bold)
-                ForEach(viewStore.episodes.indices, id: \.self) { index in
-                    let episode = viewStore.episodes[index]
-                    Button(action: {
-                        viewStore.send(.episodeCellTapped(index))
-                    }, label: {
-                        WorkEpisodeCell(episode: episode)
-                            .foregroundColor(.primary)
-                    })
-                    .sheet(item: viewStore.binding(
-                            get: \.presentation,
-                            send: { _ in .episodeDismissed } )
-                    ) { $0 }
+                ForEachStore(
+                    store.scope(state: \.episodeCellStates, action: WorkEpisodesAction.episodeCell(index:action:))
+                ) { cellStore in
+                    WorkEpisodeCell(store: cellStore)
                 }
                 if let pageInfo = viewStore.pageInfo, pageInfo.hasNextPage == true {
                     HStack {
@@ -159,7 +164,7 @@ struct WorkEpisodesView_Previews: PreviewProvider {
                                       environment: WorkEpisodesEnvironment(
                                         mainQueue: DispatchQueue.main.eraseToAnyScheduler()
                                       )
-                        )
+        )
         )
     }
 }
